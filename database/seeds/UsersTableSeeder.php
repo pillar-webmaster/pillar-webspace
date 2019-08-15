4bb02047cc919b98ccb2cdb653118affd7941c41
Aaron Angelo Vicuna <aaron_vicuna@sutd.edu.sg>
2019-08-15 15:53:47 +0800

    adding role to user seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,13 +12,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        $user = DB::table('users')->insert([
             'name' => 'Aaron Angelo Vicuna',
             'email' => 'arjieangelsences@gmail.com',
             'email_verified_at' => now(),
             'password' => bcrypt('logan9c20'),
             'created_at' => now(),
             'updated_at' => now()
+        ]);
+
+        DB::table('mode_has_roles')->insert([
+            'role_id' => 4,
+            'model_type' => 'App\User',
+            'model_id' => $user->id
         ]);
     }
 }
